dcd903380f8b8ffd9c5f3715ebec4f2d03c74c40
PhongNT <phongnt@kaopiz.com>
2023-12-12 17:41:21 +0700

    config package needed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 import { createApp } from "vue";
 import "./style.css";
+import { createPinia } from "pinia";
 import App from "./App.vue";
-import { pinia } from "./store";
 import ElementPlus from "element-plus";
 
+const pinia = createPinia();
 const app = createApp(App);
+
 app.use(pinia);
 app.use(ElementPlus);
-
 app.mount("#app");
