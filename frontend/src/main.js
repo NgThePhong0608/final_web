8b783c1f94b973f68d64ee7e791a2e62690f14d5
PhongNT <phongnt@kaopiz.com>
2023-12-13 09:22:00 +0700

    add fontawesome package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import { createApp } from "vue";
-import "./style.css";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import router from "../src/router";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const pinia = createPinia();
-const app = createApp(App);
+const app = createApp(App).component("font-awesome", FontAwesomeIcon);
 
 app.use(pinia);
 app.use(ElementPlus);
